ca738efb37604eef9453c0e531faf46d5ed95403
{sasaya87} <{jsa80506@gmail.com}>
2019-12-15 20:26:58 +0900

    initial commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int Height;
+    public int Width;
+    public int[,] Map;
+    private int soil=0;
+    private int grass=1;
+    private int wall=2;
+    private int mtn=4;
+    public GameObject Soil;
+    public GameObject Grass;
+    public GameObject Wall;
+    public GameObject Mtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetMapData();
+        CreateMapData();
+        
+    }
+
+    private void ResetMapData(){
+
+        Map = new int[Height, Width];
+        for (int i = 0; i < Height; i++) {
+            for (int j = 0; j < Width; j++) {
+                Map[i, j] = 0;
+            }
+        }
+    }
+
+    private void CreateMapData() {
+        for (int i = 0; i < Height; i++) {
+            for (int j = 0; j < Width; j++) {
+                if (Map[i, j] == soil) {
+                    Instantiate(Soil, new Vector3(j - Width/2, i - Height/2, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool IsObstacle;
+    public bool IsSoil;
+    public bool IsPassable;
+    public bool IsBrekable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetSetItem(){
+        
+    } 
+}
